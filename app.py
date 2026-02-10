c4f809ff0a1dc285b2011e70835b0e8d43355bdd
admin <admin@localhost.localdomain>
2026-02-10 15:49:07 +0530

    bug created

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def add(a, b):
     return a + b
 
-def multiply(a, b):
-    return a * b
+#def multiply(a, b):
+#    return a * b
 
 if __name__ == "__main__":
     print("CI/CD Demo – SUCCESS CASE")
